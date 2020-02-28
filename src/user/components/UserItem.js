c5ecf6a41b93cd1b2add2973b77ecc0259479572
ahallam <alexhallam75@gmail.com>
2020-02-27 21:12:25 -0500

    Added UserList/component

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./UserItem.css";
+
+const UserItem = props => {
+  return <h2>User Item</h2>;
+};
+
+export default UserItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import UserList from "../components/UserList";
+
 const Users = () => {
-  return <h2>Users</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Bob Smith",
+      image:
+        "https://media1.s-nbcnews.com/i/newscms/2019_29/2936201/190619-joe_murphy-byline2436_341b585a5f99c3de69c34b262283c983.jpg",
+      places: 3
+    }
+  ];
+  return <UserList items={USERS} />;
 };
 
 export default Users;
