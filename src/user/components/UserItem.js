b27b5dbc8fcd6b248c7f851974efbc77673585da
ahallam <alexhallam75@gmail.com>
2020-02-27 21:23:38 -0500

    Added UserItem component

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -2,7 +2,23 @@ import React from "react";
 import "./UserItem.css";
 
 const UserItem = props => {
-  return <h2>User Item</h2>;
+  return (
+    <li className="user-item">
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <img src={props.image} alt={props.name} />
+        </div>
+        <div className="user-item__info">
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+            </h3>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default UserItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -4,13 +4,13 @@ import UserList from "../components/UserList";
 
 const Users = () => {
   const USERS = [
-    {
-      id: "u1",
-      name: "Bob Smith",
-      image:
-        "https://media1.s-nbcnews.com/i/newscms/2019_29/2936201/190619-joe_murphy-byline2436_341b585a5f99c3de69c34b262283c983.jpg",
-      places: 3
-    }
+    // {
+    //   id: "u1",
+    //   name: "Bob Smith",
+    //   image:
+    //     "https://media1.s-nbcnews.com/i/newscms/2019_29/2936201/190619-joe_murphy-byline2436_341b585a5f99c3de69c34b262283c983.jpg",
+    //   places: 3
+    // }
   ];
   return <UserList items={USERS} />;
 };
